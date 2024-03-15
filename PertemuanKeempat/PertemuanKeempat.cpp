7f2b21c3ae54ff4003528b86f7396a41d812f87e
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-03-15 10:57:36 +0700

    membuat input function

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
+int n;
+int arr[11];
 
-void insertionSort() {
-	int j, i, n, temp;
-	int	arr[11];
+void input() {
+	while (true) {
+		cout << "Masukan banyak elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "===================" << endl;
 
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
+
