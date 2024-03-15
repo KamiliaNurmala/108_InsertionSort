1e3a024e4f7be7367a887446559ebdb2e4a38ec8
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-03-15 10:58:46 +0700

    membuat display function

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -39,3 +39,14 @@ void InsertionSort() {
 	}
 }
 
+void display() {
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "===================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
