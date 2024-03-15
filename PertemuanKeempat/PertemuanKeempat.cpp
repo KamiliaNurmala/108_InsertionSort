5fb025bdf99c6ff5b948817aa8e68f2343e270cb
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-03-15 10:58:18 +0700

    membuat InsertionSort function

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -24,3 +24,18 @@ void input() {
 	}
 }
 
+void InsertionSort() {
+	int j, i, temp;
+
+	for (i = 1; i <= n - 1; i++) { // step 1
+
+		temp = arr[i]; // step 2
+		j = 1 - 1; // step 3
+		while (j >= 0 && arr[j] > temp) { // step 4
+			arr[j + 1] = arr[j]; // step 4a
+			j--; // step 4b
+		}
+		arr[j + 1] = temp; // step 5
+	}
+}
+
